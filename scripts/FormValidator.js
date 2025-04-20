7b26ea711739f3b3da8d3e74320e3a6322c2e800
Jorge Marcelin <jmarcelinr@outlook.com>
2025-04-20 09:11:45 -0600

    Fix of the FormValidator class

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -24,6 +24,7 @@ export default class FormValidator {
   }
 
   _setEventListeners() {
+    this._toggleButtonState();
     this._inputList.forEach((inputElement) => {
       inputElement.addEventListener("input", () => {
         this._checkInputValidity(inputElement);
