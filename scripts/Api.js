46a5f126d6c06eee93b558e6281a3a086c261c7b
Jorge Marcelin <jmarcelinr@outlook.com>
2025-05-23 10:14:09 -0600

    Se elimina el console.log de la función que comprobaba la respuesta de la API

--- scripts/Api.js
+++ scripts/Api.js
@@ -66,7 +66,6 @@ export default class Api {
 
   _checkResponse(res) {
     if (res.ok) {
-      console.log(res);
       return res.json();
     }
     return Promise.reject(`Error: ${res.status}`);
